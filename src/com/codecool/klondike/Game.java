7fa217e820d77618f64dff0ce210a0f33f0d196c
juhaszTamas <juhasz.tamas991@gmail.com>
2017-08-22 09:31:32 +0200

    nullpointer error bug fixed - start branching from here

--- src/com/codecool/klondike/Game.java
+++ src/com/codecool/klondike/Game.java
@@ -90,7 +90,7 @@ public class Game extends Pane {
             handleValidMove(card, pile);
         } else {
             draggedCards.forEach(MouseUtil::slideBack);
-            draggedCards = null;
+            draggedCards.clear();
         }
     };
 
